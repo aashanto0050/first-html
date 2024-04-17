3d6496883a6636b26bd8bb57b25923e55e82f0e6
aashanto0050 <md.asrafulalam@Mds-MacBook-Air.local>
2024-04-17 22:03:16 +0600

    first commit

---
+++ first.c
@@ -0,0 +1,15 @@
+
+
+
+#include <stdio.h>
+
+int main()
+{
+    int final_price=100;
+    float hastage='#';
+    printf("The question is - \"How to write a \n\\comment/ in C programming language?\"\n");
+    return 0;
+    printf("the question is -\"how to write a \n\\commant/ in c programming language?\"\n");
+}
+
+
